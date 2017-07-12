34457afaeccdb1a6be25667620d803b50d783ddd
Florence <florence@hotmail.fr>
2017-07-12 17:00:04 +0200

    finiiish

--- sql.sql
+++ sql.sql
@@ -16,6 +16,31 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `commentaire`
+--
+
+DROP TABLE IF EXISTS `commentaire`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `commentaire` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `commentaire` varchar(200) DEFAULT NULL,
+  `username` varchar(200) DEFAULT NULL,
+  `datecreate` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `commentaire`
+--
+
+LOCK TABLES `commentaire` WRITE;
+/*!40000 ALTER TABLE `commentaire` DISABLE KEYS */;
+/*!40000 ALTER TABLE `commentaire` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `forums`
 --
 
@@ -29,7 +54,7 @@ CREATE TABLE `forums` (
   `username` varchar(200) DEFAULT NULL,
   `datecreate` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +63,6 @@ CREATE TABLE `forums` (
 
 LOCK TABLES `forums` WRITE;
 /*!40000 ALTER TABLE `forums` DISABLE KEYS */;
-INSERT INTO `forums` VALUES (27,'  Je pense que j\'ai fais pas mal de choses  c\'est plutot cool','C\'est l\'éclate','jean-pierre','2017-07-11 16:54:24'),(28,' Bon y a quand meme des modifications a faire encore ! mais on y est presque ','','Florence','2017-07-11 16:55:17');
 /*!40000 ALTER TABLE `forums` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,7 +81,7 @@ CREATE TABLE `questions` (
   `username` varchar(250) DEFAULT NULL,
   `domaine` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,7 +90,7 @@ CREATE TABLE `questions` (
 
 LOCK TABLES `questions` WRITE;
 /*!40000 ALTER TABLE `questions` DISABLE KEYS */;
-INSERT INTO `questions` VALUES (44,NULL,'ça va sinon?\r\n\r\n      ','2017-07-11 16:52:33','Florence',NULL),(45,NULL,'ça va et toi ??\r\n\r\n      ','2017-07-11 16:53:02','jean-pierre',NULL),(46,NULL,'ça va ! je suis crevé ! j ai l impression d avoir rein fait pourtant \r\n\r\n\r\n\r\n      ','2017-07-11 16:55:47','Florence',NULL);
+INSERT INTO `questions` VALUES (88,NULL,'bonjour\r\n\r\n      ','2017-07-12 16:20:20','Array',NULL),(89,NULL,'\r\nd\r\n      ','2017-07-12 16:20:27','Array',NULL),(90,NULL,'d\r\n\r\n      ','2017-07-12 16:20:29','Array',NULL),(91,NULL,'d\r\n\r\n      ','2017-07-12 16:20:31','Array',NULL),(92,NULL,'\r\nd\r\n      ','2017-07-12 16:20:32','Array',NULL),(93,NULL,'d\r\n\r\n      ','2017-07-12 16:20:33','Array',NULL),(94,NULL,'\r\n\r\n      ','2017-07-12 16:20:34','Array',NULL),(95,NULL,NULL,'2017-07-12 16:36:56','Array',NULL),(96,NULL,NULL,'2017-07-12 16:37:16','Array',NULL),(97,NULL,NULL,'2017-07-12 16:37:35','Array',NULL),(98,NULL,NULL,'2017-07-12 16:38:24','Array',NULL),(99,'qqq','Array','2017-07-12 16:45:31',NULL,NULL),(100,'sssss','Array','2017-07-12 16:45:38',NULL,NULL),(101,'dfgdfh','Array','2017-07-12 16:47:10',NULL,NULL),(102,NULL,'scf\r\n\r\n      ','2017-07-12 16:56:17','Array',NULL),(103,NULL,'\r\n\r\n      ','2017-07-12 16:56:17','Array',NULL);
 /*!40000 ALTER TABLE `questions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,7 +107,7 @@ CREATE TABLE `user` (
   `email` varchar(255) DEFAULT NULL,
   `uPassword` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -92,7 +116,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (59,'Florence','florence-mar@hotmail.fr','florence'),(60,'Florence','florence-mar@hotmail.fr','florence'),(61,'jean-pierre','jeanpierre@hotmail.fr','jean');
+INSERT INTO `user` VALUES (59,'Florence','florence-mar@hotmail.fr','florence'),(60,'Florence','florence-mar@hotmail.fr','florence'),(61,'jean-pierre','jeanpierre@hotmail.fr','jean'),(62,'imrik','pierre.mar.66000@gmail.com','admin'),(63,'Deihix','rolfe.allan8@gmail.com','4pa5s2Dx');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -105,4 +129,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-11 17:02:47
+-- Dump completed on 2017-07-12 16:59:49
